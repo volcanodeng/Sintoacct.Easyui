19917d8ebd25b6d7afbac3379f8bca1678d99824
邓庆 <3035112@qq.com>
2016-08-12 21:43:53 +0800

    初始提交

---
+++ Scripts/easyui.extend.js
@@ -0,0 +1,89 @@
+﻿
+// extend the 'telNum' rule
+$.extend($.fn.validatebox.defaults.rules, {
+    telNum: {
+        validator: function (value, param) {
+            return (/0\d{2,3}-\d{5,9}|0\d{2,3}-\d{5,9}/g).test(value);
+        },
+        message: '电话号码不匹配。'
+    }
+});
+
+// extend the 'mobile' rule
+$.extend($.fn.validatebox.defaults.rules, {
+    mobile: {
+        validator: function (value, param) {
+            return (/^1[3|4|5|7|8]\d{9}$/g).test(value);
+        },
+        message: '手机号码无效。'
+    }
+});
+
+
+// extend the 'equals' rule
+$.extend($.fn.validatebox.defaults.rules, {
+    equals: {
+        validator: function (value, param) {
+            return value == $(param[0]).val();
+        },
+        message: '输入内容不一致。'
+    }
+});
+
+
+
+
+//单元格编辑
+$.extend($.fn.datagrid.methods, {
+    editCell: function (jq, param) {
+        return jq.each(function () {
+            var opts = $(this).datagrid('options');
+            var fields = $(this).datagrid('getColumnFields', true).concat($(this).datagrid('getColumnFields'));
+            for (var i = 0; i < fields.length; i++) {
+                var col = $(this).datagrid('getColumnOption', fields[i]);
+                col.editor1 = col.editor;
+                if (fields[i] != param.field) {
+                    col.editor = null;
+                }
+            }
+            $(this).datagrid('beginEdit', param.index);
+            var ed = $(this).datagrid('getEditor', param);
+            if (ed) {
+                if ($(ed.target).hasClass('textbox-f')) {
+                    $(ed.target).textbox('textbox').focus();
+                } else {
+                    $(ed.target).focus();
+                }
+            }
+            for (var i = 0; i < fields.length; i++) {
+                var col = $(this).datagrid('getColumnOption', fields[i]);
+                col.editor = col.editor1;
+            }
+        });
+    },
+    enableCellEditing: function (jq) {
+        return jq.each(function () {
+            var dg = $(this);
+            var opts = dg.datagrid('options');
+            opts.oldOnClickCell = opts.onClickCell;
+            opts.onClickCell = function (index, field) {
+                if (opts.editIndex != undefined) {
+                    if (dg.datagrid('validateRow', opts.editIndex)) {
+                        //自定义的赋值函数
+                        if (typeof (fieldSettingFun) == "function") fieldSettingFun(opts.editIndex);
+                        dg.datagrid('endEdit', opts.editIndex);
+                        opts.editIndex = undefined;
+                    } else {
+                        return;
+                    }
+                }
+                dg.datagrid('selectRow', index).datagrid('editCell', {
+                    index: index,
+                    field: field
+                });
+                opts.editIndex = index;
+                opts.oldOnClickCell.call(this, index, field);
+            }
+        });
+    }
+});
