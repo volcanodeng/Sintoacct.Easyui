aac6ec090b3c12727d30503c02909692fb753a6c
Administrator <Administrator@WIN-46287PIU2SL>
2016-12-02 00:46:55 +0800

    增加common.js

---
+++ Scripts/common.js
@@ -0,0 +1,14 @@
+﻿
+function formSuccessHandle(data)
+{
+    if (data == null) return;
+    var data = JSON.parse(data);
+
+    if (data.message != null) {
+        $.messager.alert("保存", data.message, "warning");
+        return;
+    }
+    else {
+        $.messager.show({ title: "保存", msg: "保存成功" });
+    }
+}
