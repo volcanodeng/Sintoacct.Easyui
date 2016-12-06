7cc2b0c852973170b842741639448af7a4b59035
邓庆 <3035112@qq.com>
2016-12-06 10:28:14 +0800

    公共的消息响应方法

--- Scripts/common.js
+++ Scripts/common.js
@@ -1,14 +1,15 @@
 ﻿
 function formSuccessHandle(data)
 {
-    if (data == null) return;
+    if (data == null) return false;
     var data = JSON.parse(data);
 
     if (data.message != null) {
         $.messager.alert("保存", data.message, "warning");
-        return;
+        return false;
     }
     else {
         $.messager.show({ title: "保存", msg: "保存成功" });
+        return true;
     }
 }
