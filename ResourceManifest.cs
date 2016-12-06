18a63479f7c0d1f053d58bccc4b2dc860ba44be1
邓庆 <3035112@qq.com>
2016-12-06 18:06:57 +0800

    修改脚本文件读取方式，将打开页签的脚本放入common.js

---
+++ ResourceManifest.cs
@@ -0,0 +1,19 @@
+﻿using Orchard.UI.Resources;
+
+namespace Sintoacct.Easyui
+{
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            manifest.DefineScript("Easyui.min").SetUrl("jquery.easyui.min.js").SetDependencies("jQuery");
+            manifest.DefineScript("Easyui.lang").SetUrl("easyui-lang-zh_CN.js").SetDependencies("Easyui.min");
+            manifest.DefineScript("Easyui.extend").SetUrl("easyui.extend.js").SetDependencies("Easyui.lang");
+            manifest.DefineScript("Easyui").SetUrl("common.js").SetDependencies("Easyui.extend");
+
+            manifest.DefineStyle("site").SetUrl("site.css");
+            manifest.DefineStyle("Easyui").SetUrl("easyui.css");
+            manifest.DefineStyle("Easyui.icon").SetUrl("icon.css");
+        }
+    }
+}
--- Scripts/common.js
+++ Scripts/common.js
@@ -1,4 +1,61 @@
 ﻿
+function AddTabContent(title, content) {
+    if ($("#tab_wrapper").tabs("exists", title)) {
+        $("#tab_wrapper").tabs("select", title);
+        return;
+    }
+
+    $("#tab_wrapper").tabs('add', {
+        title: title,
+        content: content,
+        closable: true,
+        selected: true
+    });
+}
+
+function AddTabHref(title, href) {
+
+    if ($("#tab_wrapper").tabs("exists", title)) {
+        $("#tab_wrapper").tabs("select", title);
+        return;
+    }
+
+    $("#tab_wrapper").tabs('add', {
+        title: title,
+        href: href,
+        closable: true,
+        selected: true
+    });
+
+}
+
+function AddTabIframe(title, href) {
+
+    if ($("#tab_wrapper").tabs("exists", title)) {
+        $("#tab_wrapper").tabs("select", title);
+        return;
+    }
+
+    $("#tab_wrapper").tabs('add', {
+        title: title,
+        content: '<iframe scrolling="yes" frameborder="0"  src="' + href + '" style="width:100%;height:100%;"></iframe>',
+        closable: true,
+        selected: true
+    });
+
+}
+
+function removePanel() {
+    var tab = $('#tab_wrapper').tabs('getSelected');
+    if (tab) {
+        var index = $('#tab_wrapper').tabs('getTabIndex', tab);
+        $('#tab_wrapper').tabs('close', index);
+    }
+}
+
+
+
+
 function formSuccessHandle(data)
 {
     if (data == null) return false;
